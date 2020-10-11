f29f2de1d08714cba1fcaf36ed0ee5c5e57fa956
taehoon.kim <taehoon7289@gmail.com>
2020-10-12 02:27:03 +0900

    init commit

---
+++ src/components/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <span>test 컴포넌트 영역 :: {{ data.value }}</span>
+</template>
+
+<script>
+import { ref } from "vue";
+
+const useCount = () => {
+  const count = ref(0);
+  return { count };
+};
+
+export default {
+  name: "test",
+  props: {
+    value: {
+      type: [Object]
+    }
+  },
+  async setup() {
+    const { count } = useCount()
+    fnTest
+    return {
+      count
+    }
+  },
+  methods: {
+    fnTest(val) {
+      return this.$dayjs(val).format("YYYY-MM-DD HH:mm:ss");
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,10 +2,11 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h1>{{ data.text }}</h1>
-    <h1>{{ value }}</h1>
+    <h1>{{ data.value }}</h1>
     <h1>{{ data.test }}</h1>
     <h1>{{ $dayjs().format("YYYY-MM-DD") }}</h1>
   </div>
+  <test :value="data.value"></test>
 </template>
 
 <script>
@@ -22,67 +23,57 @@ import {
   onDeactivated,
   onErrorCaptured
 } from "vue";
+import Test from "@/components/test";
+
+import dayjs from "dayjs";
 
 export default {
   name: "about",
-  props: {
-    value: {
-      type: [String],
-      default: "dddd"
-    }
-  },
-  data: function() {
-    console.log("this11", this);
-    console.log("this22", this.$dayjs());
-    console.log("this22", this.$router);
-    return {
-      one: 1
-    };
-  },
+  components: { Test },
   setup(props, { attrs, slots, emit }) {
     console.log("attrs", attrs);
     console.log("slots", slots);
     console.log("emit", emit);
-    console.log("props.value", props.value);
     const data = reactive({
       text: "aaaaaaaaaaaa",
-      test: computed(() => data.text.toUpperCase())
+      test: computed(() => data.text.toUpperCase()),
+      value: dayjs()
     });
     onBeforeMount(() => {
       // ...
-      console.log('onBeforeMount', this)
+      console.log("onBeforeMount", this);
     });
     onMounted(() => {
       // ...
-      console.log('onMounted', this)
+      console.log("onMounted", this);
     });
     onBeforeUpdate(() => {
       // ...
-      console.log('onBeforeUpdate', this)
+      console.log("onBeforeUpdate", this);
     });
     onUpdated(() => {
       // ...
-      console.log('onUpdated', this)
+      console.log("onUpdated", this);
     });
     onBeforeUnmount(() => {
       // ...
-      console.log('onBeforeUnmount', this)
+      console.log("onBeforeUnmount", this);
     });
     onUnmounted(() => {
       // ...
-      console.log('onUnmounted', this)
+      console.log("onUnmounted", this);
     });
     onActivated(() => {
       // ...
-      console.log('onActivated', this)
+      console.log("onActivated", this);
     });
     onDeactivated(() => {
       // ...
-      console.log('onDeactivated', this)
+      console.log("onDeactivated", this);
     });
     onErrorCaptured(() => {
       // ...
-      console.log('onErrorCaptured', this)
+      console.log("onErrorCaptured", this);
     });
     return {
       data
