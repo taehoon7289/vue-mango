d9c8ea2c39ae29c24eef7e0ce21efd7b2fd38057
taehoon.kim <taehoon7289@gmail.com>
2020-11-07 19:34:18 +0900

    commit

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,8 @@ module.exports = {
       // 프록시 요청을 보낼 api의 시작 부분
       "/api": {
         // 프록시 요청을 보낼 서버의 주소
-        target: "http://34.64.203.83:58080"
+        // target: "http://34.64.203.83:58080"
+        target: "http://127.0.0.1:58080"
       }
     },
     https: true
